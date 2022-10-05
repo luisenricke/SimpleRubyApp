c03bc83ae8154ac30d2bbf15566b1163d181d100
Luis Villalobos <luisevm24@outlook.com>
2022-10-05 04:48:01 -0500

    fix: update form and route of signup
    - Update style of form
    - Configuration of form with bootstrap_form_for
    - Update reference migration of the table User
    - Update route from /users/sign_up to /signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   root to: 'pages#index'
 
   # Auth routes
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
 end
