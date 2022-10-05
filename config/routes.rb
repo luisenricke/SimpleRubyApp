51c4de1103e2529b3d96fa2ff8ddca284266c045
Luis Villalobos <luisevm24@outlook.com>
2022-10-05 04:10:40 -0500

    fix: migrate route from /users/sign_in to /login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'pages#index'
+
+  # Auth routes
+  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
