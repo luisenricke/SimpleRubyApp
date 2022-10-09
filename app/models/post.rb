ce364d47bd3ef05ab5285550ea28a53049e8324d
Luis Villalobos <luisevm24@outlook.com>
2022-10-09 02:43:24 -0500

    feat: search with differents scopes of the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  default_scope -> { includes(:user).order(created_at: :desc) }
+
+  scope :by_category, lambda { |branch, category_name|
+    joins(:category).where(categories: { name: category_name, branch: branch })
+  }
+
+  scope :by_branch, lambda { |branch|
+    joins(:category).where(categories: { branch: branch })
+  }
+
+  scope :search, lambda { |search|
+    where('LOWER(title) LIKE LOWER(?) OR LOWER(content) LIKE LOWER(?)', "%#{search}%", "%#{search}%")
+  }
 end
