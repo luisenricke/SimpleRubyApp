887891309e7420ad782b49163dd7a104ec42f0f8
Luis Villalobos <luisevm24@outlook.com>
2022-10-10 01:09:14 -0500

    feat: add destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,6 +51,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def get_posts
